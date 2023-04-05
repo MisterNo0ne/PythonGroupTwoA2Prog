e136b9c49669e48b58e0106eaf0ded2d3fa74a70
MisterNo0ne <84687450+MisterNo0ne@users.noreply.github.com>
2023-04-05 09:48:33 -0600

    Updated npc.py and player.py

---
+++ mainThing/player.py
@@ -0,0 +1,7 @@
+class player:
+    def __init__():
+        knownElements = [True, True, False, True]
+        itemsOwned = [] #items is already a thing ig
+    
+    def printValues():
+        println(str(knownElements[0]) + str(knownElements[1]))
